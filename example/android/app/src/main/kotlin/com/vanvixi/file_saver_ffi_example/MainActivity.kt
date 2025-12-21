6bc489f6e0d4255b2ceda7447d14841be059ffb8
vanvixi <vanvixi.dev@gmail.com>
2025-12-21 21:48:31 +0700

    init project

---
+++ example/android/app/src/main/kotlin/com/vanvixi/file_saver_ffi_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vanvixi.file_saver_ffi_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Classes/file_saver_ffi.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../file_saver_ffi.podspec for more information.
+#include "../../src/file_saver_ffi.c"
