4f5e674de5c78f5a306677467c545de54d4b8066
vanvixi <vanvixi.dev@gmail.com>
2025-12-24 11:20:34 +0700

    refactor: simplify SaveResult conversion to return Uri directly

--- lib/src/platforms/android/file_saver_android.dart
+++ lib/src/platforms/android/file_saver_android.dart
@@ -6,7 +6,6 @@ import 'package:jni/jni.dart';
 import '../../exceptions/file_saver_exceptions.dart';
 import '../../models/conflict_resolution.dart';
 import '../../models/file_type.dart';
-import '../../models/save_result.dart';
 import '../../platform_interface/file_saver_platform.dart';
 import 'bindings.g.dart' as bindings;
 
@@ -24,7 +23,7 @@ class FileSaverAndroid extends FileSaverPlatform {
   }
 
   @override
-  Future<SaveResult> saveBytes({
+  Future<Uri> saveBytes({
     required Uint8List fileBytes,
     required String fileName,
     required FileType fileType,
@@ -34,7 +33,6 @@ class FileSaverAndroid extends FileSaverPlatform {
     _validateInput(fileBytes, fileName);
 
     try {
-      // Convert Dart types to JNI types
       final jByteArray = JByteArray.from(fileBytes);
       final jFileName = fileName.toJString();
       final jExtension = fileType.ext.toJString();
@@ -51,50 +49,32 @@ class FileSaverAndroid extends FileSaverPlatform {
         jConflictMode,
       );
 
-      // Convert Kotlin SaveResult to Dart SaveResult
-      return _convertSaveResult(kotlinResult);
+      return _convertToUriOrThrow(kotlinResult);
     } catch (e) {
       throw FileSaverException.fromObj(e);
     }
   }
 
-  // ===========================================
-  // Helper Methods
-  // ===========================================
-
-  /// Converts Kotlin SaveResult to Dart SaveResult
-  SaveResult _convertSaveResult(bindings.SaveResult kotlinResult) {
-    if (kotlinResult.isSuccess()) {
-      final filePathJStr = kotlinResult.getFilePath();
-      final uriJStr = kotlinResult.getUri();
-
-      final filePath = filePathJStr?.toDartString(releaseOriginal: true) ?? '';
-      final uri = uriJStr?.toDartString(releaseOriginal: true) ?? '';
-
-      // Release Kotlin object
+  Uri _convertToUriOrThrow(bindings.SaveResult kotlinResult) {
+    if (!kotlinResult.isSuccess()) {
+      final errCodeJStr = kotlinResult.getErrorCode();
+      final errMsgJStr = kotlinResult.getErrorMessage();
+      final errorCode = errCodeJStr?.toDartString(releaseOriginal: true);
+      final errMsg = errMsgJStr?.toDartString(releaseOriginal: true);
       kotlinResult.release();
 
-      return SaveSuccess(filePath: filePath, uri: uri);
+      throw FileSaverException.fromErrorResult(
+        errorCode ?? 'UNKNOWN',
+        errMsg ?? 'Unknown error',
+      );
     }
 
-    // Failure case
-    final errCodeJStr = kotlinResult.getErrorCode();
-    final errMsgJStr = kotlinResult.getErrorMessage();
-
-    final errorCode =
-        errCodeJStr?.toDartString(releaseOriginal: true) ?? 'UNKNOWN';
-    final errMsg =
-        errMsgJStr?.toDartString(releaseOriginal: true) ?? 'Unknown error';
-
-    // Release Kotlin object
+    final uriJStr = kotlinResult.getUri();
+    final uriString = uriJStr?.toDartString(releaseOriginal: true) ?? '';
     kotlinResult.release();
 
-    return SaveFailure(error: errMsg, errorCode: errorCode);
+    return Uri.parse(uriString);
   }
-
-  // ===========================================
-  // Validation Methods
-  // ===========================================
 
   void _validateInput(Uint8List bytes, String fileName) {
     if (bytes.isEmpty) {
