381addc79349910dbac2ece5a82d5292439a610f
vanvixi <vanvixi.dev@gmail.com>
2025-12-24 23:36:44 +0700

    refactor: change FileType properties to mutable and normalize values in init block

--- android/src/main/kotlin/com/vanvixi/file_saver_ffi/models/FileType.kt
+++ android/src/main/kotlin/com/vanvixi/file_saver_ffi/models/FileType.kt
@@ -2,9 +2,15 @@ package com.vanvixi.file_saver_ffi.models
 
 
 data class FileType(
-    val ext: String,
-    val mimeType: String
+    var ext: String,
+    var mimeType: String
 ) {
+
+    init {
+        ext = ext.lowercase()
+        mimeType = mimeType.lowercase()
+    }
+
     enum class Category {
         IMAGE,
         VIDEO,
@@ -20,87 +26,9 @@ data class FileType(
             else -> Category.CUSTOM
         }
 
-    val isImage: Boolean
-        get() = category == Category.IMAGE
-
-
-    val isVideo: Boolean
-        get() = category == Category.VIDEO
-
-
-    val isAudio: Boolean
-        get() = category == Category.AUDIO
+    val isImage get() = category == Category.IMAGE
+    val isVideo get() = category == Category.VIDEO
+    val isAudio get() = category == Category.AUDIO
 }
-
-
-//val FileType.isImage: Boolean
-//    get() = this is ImageType || mimeType.startsWith("image/")
-//
-//val FileType.isVideo: Boolean
-//    get() = this is VideoType || mimeType.startsWith("video/")
-//
-//val FileType.isAudio: Boolean
-//    get() = this is AudioType || mimeType.startsWith("audio/")
-
-//enum class ImageType(
-//    override val ext: String,
-//    override val mimeType: String
-//) : FileType {
-//
-//    PNG("png", "image/png"),
-//    JPG("jpg", "image/jpeg"),
-//    JPEG("jpeg", "image/jpeg"),
-//    GIF("gif", "image/gif"),
-//    WEBP("webp", "image/webp"),
-//    BMP("bmp", "image/bmp"),
-//    HEIC("heic", "image/heic"),
-//    HEIF("heif", "image/heif"),
-//    TIFF("tiff", "image/tiff"),
-//    TIF("tif", "image/tiff"),
-//    ICO("ico", "image/x-icon"),
-//    DNG("dng", "image/x-adobe-dng");
-//}
-//
-//enum class VideoType(
-//    override val ext: String,
-//    override val mimeType: String
-//) : FileType {
-//
-//    MP4("mp4", "video/mp4"),
-//    THREE_GP("3gp", "video/3gpp"),
-//    WEBM("webm", "video/webm"),
-//    M4V("m4v", "video/x-m4v"),
-//    MKV("mkv", "video/x-matroska"),
-//    MOV("mov", "video/quicktime"),
-//    AVI("avi", "video/x-msvideo"),
-//    FLV("flv", "video/x-flv"),
-//    WMV("wmv", "video/x-ms-wmv"),
-//    HEVC("hevc", "video/hevc"),
-//    VP9("vp9", "video/x-vnd.on2.vp9"),
-//    AV1("av1", "video/av01");
-//}
-//
-//enum class AudioType(
-//    override val ext: String,
-//    override val mimeType: String
-//) : FileType {
-//
-//    MP3("mp3", "audio/mpeg"),
-//    AAC("aac", "audio/aac"),
-//    WAV("wav", "audio/wav"),
-//    AMR("amr", "audio/amr"),
-//    THREE_GP("3gp", "audio/3gpp"),
-//    M4A("m4a", "audio/mp4"),
-//    OGG("ogg", "audio/ogg"),
-//    FLAC("flac", "audio/flac"),
-//    OPUS("opus", "audio/opus"),
-//    AIFF("aiff", "audio/aiff"),
-//    CAF("caf", "audio/x-caf");
-//}
-//
-//data class CustomFileType(
-//    override val ext: String,
-//    override val mimeType: String
-//) : FileType
 
 
