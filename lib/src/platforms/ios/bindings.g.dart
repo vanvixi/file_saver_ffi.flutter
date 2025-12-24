6b7473d9d014a30dbd7171199a03e551bd328e37
vanvixi <vanvixi.dev@gmail.com>
2025-12-24 14:11:57 +0700

    feat(ios): update FFI bindings for FileSaver with documentation

--- lib/src/platforms/ios/bindings.g.dart
+++ lib/src/platforms/ios/bindings.g.dart
@@ -8,17 +8,21 @@
 // ignore_for_file: type=lint, unused_import
 import 'dart:ffi' as ffi;
 
-class NativeLibrary {
+/// Bindings for iOS `file_saver_ffi.h`.
+///
+/// Regenerate bindings with `fvm dart run ffigen --config ffigen.yaml`.
+///
+class FileSaverFfiBindings {
   /// Holds the symbol lookup function.
   final ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName)
   _lookup;
 
   /// The symbols are looked up in [dynamicLibrary].
-  NativeLibrary(ffi.DynamicLibrary dynamicLibrary)
+  FileSaverFfiBindings(ffi.DynamicLibrary dynamicLibrary)
     : _lookup = dynamicLibrary.lookup;
 
   /// The symbols are looked up with [lookup].
-  NativeLibrary.fromLookup(
+  FileSaverFfiBindings.fromLookup(
     ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName) lookup,
   ) : _lookup = lookup;
 
